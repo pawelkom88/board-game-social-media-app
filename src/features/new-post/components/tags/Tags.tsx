1b901a284538fe543faffe55a07acfd745244cf1
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-10-06 14:51:33 +0100

    chore: Add tags component

--- src/features/new-post/components/tags/Tags.tsx
+++ src/features/new-post/components/tags/Tags.tsx
@@ -5,11 +5,48 @@ import {
   FormLabel,
   FormMessage,
 } from "@/shared/components/ui/form";
-import { Input } from "@/shared/components/ui/input";
+import { Tag, TagInput } from "emblor";
+import { useState } from "react";
 import { FormControlProps } from "../../utils/types";
 
-function Tags({ formControl }: FormControlProps) {
-  // TAGS LOGIC HERE
+const fieldName = "postTags";
+type PostTag = typeof fieldName;
+
+export const defaultTags = [
+  {
+    id: "1",
+    text: "Tag 1",
+  },
+  {
+    id: "2",
+    text: "Tag 2",
+  },
+  {
+    id: "3",
+    text: "Tag 3",
+  },
+];
+
+interface TagInputProps extends FormControlProps {
+  onValueChange: (text: PostTag, tags: Tag[]) => void;
+  onError: (error: PostTag, message: { type: string; message: string }) => void;
+}
+function Tags({ formControl, onValueChange, onError }: TagInputProps) {
+  const [tags, setTags] = useState<Tag[]>(defaultTags);
+
+  const handleTagsSelect = (newTags: Tag[]) => {
+    if (!newTags.length) {
+      onError(fieldName, {
+        type: "manual",
+        message: "At least one tag is required",
+      });
+      return;
+    }
+
+    setTags(newTags);
+    onValueChange(fieldName, newTags);
+  };
+
   return (
     <FormField
       control={formControl}
@@ -18,7 +55,23 @@ function Tags({ formControl }: FormControlProps) {
         <FormItem>
           <FormLabel>Tags</FormLabel>
           <FormControl>
-            <Input placeholder="Tags" {...field} />
+            <TagInput
+              {...field}
+              className="font-bold placeholder:text-primary"
+              placeholder="Remove irrelevant tags"
+              tags={tags}
+              setTags={(newTags) => handleTagsSelect(newTags as Tag[])}
+              activeTagIndex={0}
+              setActiveTagIndex={() => {}}
+              maxTags={3}
+              showCount
+              placeholderWhenFull="Maximum 3 tags"
+              styleClasses={{
+                tag: {
+                  body: "bg-primary/70 text-white border-none font-bold pl-3",
+                },
+              }}
+            />
           </FormControl>
           <FormMessage />
         </FormItem>
