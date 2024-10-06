9c0b78ac53b9311576d6dfd2283d38eb919b105d
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-10-06 14:50:55 +0100

    style: improve form related items styles

--- src/shared/components/ui/form.tsx
+++ src/shared/components/ui/form.tsx
@@ -92,7 +92,7 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && "text-destructive", className)}
+      className={cn(error && "text-destructive", `text-lg ${className}`)}
       htmlFor={formItemId}
       {...props}
     />
@@ -152,7 +152,10 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-[0.8rem] font-medium text-destructive", className)}
+      className={cn(
+        "text-[0.9rem] bg-red-700 text-white w-max px-2 rounded-md",
+        className
+      )}
       {...props}
     >
       {body}
