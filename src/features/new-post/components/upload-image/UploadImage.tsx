4e92f9edacd5e91fe63c20a9f76e727c4e73c919
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-10-06 14:51:56 +0100

    refactor: Refactor UpoadImage component

--- src/features/new-post/components/upload-image/UploadImage.tsx
+++ src/features/new-post/components/upload-image/UploadImage.tsx
@@ -6,7 +6,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/shared/components/ui/tooltip";
-import { Upload } from "lucide-react";
+import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 import { FormControlProps } from "../../utils/types";
 
 export function UploadImage({ formControl }: FormControlProps) {
@@ -16,26 +16,25 @@ export function UploadImage({ formControl }: FormControlProps) {
       name="postImage"
       render={({ field }) => (
         <FormItem>
-          <FormLabel>
+          <FormLabel className="flex items-center">
             Upload an image
             <TooltipProvider>
               <Tooltip delayDuration={0}>
                 <TooltipContent className="bg-primary text-white p-1 rounded-sm">
                   Image can be....
                 </TooltipContent>
-                <TooltipTrigger className="ml-4">icon here</TooltipTrigger>
+                <TooltipTrigger className="ml-4">
+                  <QuestionMarkCircledIcon className="h-5 w-5" />
+                </TooltipTrigger>
               </Tooltip>
             </TooltipProvider>
           </FormLabel>
           <FormControl>
-            <div className="flex items-center">
-              <Upload className="h-8 w-8 object-cover rounded-full opacity-60" />
-              <Input
-                type="file"
-                {...field}
-                className="block w-full min-h-12 text-sm file:mr-4 file:py-2 file:px-4 file:rounded-full border-0 file:text-sm file:font-semibold file:bg-muted file:text-white hover:file:bg-accent cursor-pointer shadow-none"
-              />
-            </div>
+            <Input
+              type="file"
+              {...field}
+              className="block w-max min-h-12 text-sm file:mr-4 file:py-2 file:px-4 file:rounded-full border-0 file:text-sm file:font-semibold file:bg-muted file:text-white hover:file:bg-accent cursor-pointer shadow-noner"
+            />
           </FormControl>
         </FormItem>
       )}
